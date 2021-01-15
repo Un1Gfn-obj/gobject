5b47113ce58068b325c79bd5f4590f19cfb5aa30
Darren Ng <darren19970810@gmail.com>
2021-01-15 16:06:46 +0800

    clean up

--- viewer-file.h
+++ viewer-file.h
@@ -11,13 +11,9 @@ G_BEGIN_DECLS
 #define VIEWER_TYPE_FILE viewer_file_get_type()
 G_DECLARE_DERIVABLE_TYPE(ViewerFile,viewer_file,VIEWER,FILE,GObject)
 
-// Class structure for derivable type
+// Class structure for derivable type only
 typedef struct _ViewerFileClass {
   GObjectClass parent_class;
-  // Class virtual function fields.
-  ViewerFile *((*open)(ViewerFile*,GError**));
-  // Padding to allow adding up to 12 new virtual functions w/o breaking ABI
-  gpointer padding[12];
 } ViewerFileClass;
 
 // Public methods
