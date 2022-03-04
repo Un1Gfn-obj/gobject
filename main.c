57cad6852becfbff2ae2bae9a9f0951386fcee01
Darren Ng <Un1Gfn@gmail.com>
2022-03-05 05:27:32 +0800

    meson deps
    Signed-off-by: Darren Ng <Un1Gfn@gmail.com>

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include <openssl/evp.h>
+
+#define SZ 128
+
+int main() {
+
+  puts("encoding...");
+
+  EVP_ENCODE_CTX *ctx=EVP_ENCODE_CTX_new();
+  assert(ctx);
+  EVP_EncodeInit(ctx);
+
+  const char raw[]="hello\n";
+  char enc[SZ]={};
+  const int r=EVP_EncodeBlock((unsigned char*)enc,(const unsigned char*)raw,strlen(raw));
+  assert(r>=4&&r%4==0);
+
+  assert(0==EVP_ENCODE_CTX_num(ctx));
+  EVP_ENCODE_CTX_free(ctx);
+  ctx=NULL;
+
+  printf("%.*s\n",SZ,enc);
+
+  return 0;
+}
